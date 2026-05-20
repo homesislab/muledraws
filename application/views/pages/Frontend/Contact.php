8e4b9d879021f66892f472be49ae0106a340068f
anggy maulana ariefin <anggy.all@gmail.com>
2026-05-20 13:09:32 +0700

    feat: add new frontend pages for About, Artwork, and Contact sections along with a CI workflow configuration.

--- application/views/pages/Frontend/Contact.php
+++ application/views/pages/Frontend/Contact.php
@@ -11,7 +11,7 @@
     <title>Muledraws | Contact</title>
     <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/media/logos/favicon.ico">
     <link href="<?= base_url(); ?>assets/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/frontend/css/style.css?v=1.0.2">
 </head>
 
 <body>
