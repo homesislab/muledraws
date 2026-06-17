c7becb9d0d8e41a7fc26fee3b89b112eae9d7b57
anggy <anggy.all@gmail.com>
2026-06-18 06:53:54 +0700

    feat: initialize KTAvatar image preview in manage and upload views

---
+++ application/views/pages/Master/Work/Loader/javascript/manage.php
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+jQuery(document).ready(function() {
+    // Initialize image preview
+    new KTAvatar('kt_apps_user_add_avatar');
+});
+</script>
--- application/views/pages/Master/Work/Loader/javascript/upload.php
+++ application/views/pages/Master/Work/Loader/javascript/upload.php
@@ -2,6 +2,8 @@
 
 <script type="text/javascript">
 jQuery(document).ready(function() {
+    // Initialize image preview
+    new KTAvatar('kt_apps_user_add_avatar');
     const columns = [{
         width: 50,
         field: 'id',
